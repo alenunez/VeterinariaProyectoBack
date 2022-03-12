f388c5fcaef851f4fd2a26aa81f57a94c01e0576
alenunez097 <alenunez097@gmail.com>
2022-03-11 22:01:08 -0500

    Creacion de backEnd proyecto veterinaria

---
+++ src/main/java/com/example/VeterinariaProyectoBack/VeterinariaProyectoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.VeterinariaProyectoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaProyectoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaProyectoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VeterinariaProyectoBack/VeterinariaProyectoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VeterinariaProyectoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaProyectoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
